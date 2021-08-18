ab359eb3a6fc2227bb83408615dd09b7ff69e1ad
JSONRete <jsonrete@gmail.com>
2021-08-18 01:40:19 -0400

    commit tree 15129a16341a343ea88e6af380d3d93aeb9c94ab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your Circle class here
+const pi =  Math.PI
+const sqrt = Math.sqrt
+
+class Circle {
+    constructor(radius) {
+      this.radius = radius;
+    }
+
+    get diameter() {
+        return this.radius * 2;
+    }
+
+    get circumference() {
+        return 2 * pi * this.radius;
+    }
+
+    get area() {
+        return pi * this.radius ** 2
+    }
+
+    set diameter(diameter) {
+        this.radius = diameter / 2
+    }
+
+    set circumference(circumference) {
+        this.radius = circumference / ( pi * 2 )
+    }
+
+    set area( area ) {
+        this.radius = sqrt( area / pi )
+    }
+} 
